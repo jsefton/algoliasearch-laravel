a3865e398003029fe2e9195925f6b51f71fde51e
Jamie Sefton <jamie.sefton@parall.ax>
2017-05-24 10:58:08 +0100

    Updated namespace fix

--- src/AlgoliaServiceProvider.php
+++ src/AlgoliaServiceProvider.php
@@ -11,7 +11,7 @@ class AlgoliaServiceProvider extends ServiceProvider
     {
         $this->registerManager();
 
-        Event::subscribe('\AlgoliaSearch\Laravel\EloquentSubscriber');
+        Event::subscribe('\Jsefton\Laravel\EloquentSubscriber');
     }
 
     private function registerManager()
